3ec4979303406c454570fb293552f003388188fe
Luis Goenaga <luis.goenaga@est.iudigital.edu.co>
2024-12-23 17:34:28 -0500

    Referencias

--- components/randomfox.tsx
+++ components/randomfox.tsx
@@ -1,10 +1,11 @@
-import { JSX } from "react";
+import { JSX, useRef } from "react";
 type Props = {image: string};
 
 export const Randomfox = (props: Props): JSX.Element => {
+	const imageRef = useRef<HTMLImageElement>(null);
 	return (
 		<div>
-			<img className="rounded" width={320} height="auto" src={props.image} alt="Random Fox"/>
+			<img className="rounded" ref={imageRef} width={320} height="auto" src={props.image} alt="Random Fox"/>
 		</div>
 	);
 }
