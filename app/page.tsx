151eff0c58fa0967e4a5cd51c6c868c04bf96854
Luis Goenaga <luis.goenaga@est.iudigital.edu.co>
2024-12-23 14:20:49 -0500

    useStat y boton agregar imagen

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,50 @@
+"use client";
+
+import { useState } from "react";
 import type { NextPage } from "next";
 import Head from "next/head";
 import { Randomfox } from "../components/randomfox";
-
+type Imageitem = { id: number,  url: string };
 
 const Home: NextPage = () => {
-  const zorro = (): number => Math.floor(Math.random() * 123) + 1;
-  const image: string = `https://randomfox.ca/images/${zorro()}.jpg`;
-  return (
-    <div>
-      <Head>
-        <title>App-Goenaga</title>
-        <link rel="icon" href="/favicon.ico" />
-        <meta name="description" content="App-Goenaga" />
-      </Head>
-      <main>
-        <h1 className="text-3xl font-bold">{`Random Fox ${zorro()}`}</h1>
-        <Randomfox image={image} zorro={zorro()}/>
-      </main>
-      <footer></footer>
-    </div>
-  );
+	const zorro = (): number => Math.floor(Math.random() * 123) + 1;
+  const img = `https://randomfox.ca/images/${zorro()}.jpg`;
+
+	const [images, setImages] = useState<Imageitem[]>([
+		{ id: zorro(), url: img },
+
+	]);
+	return (
+		<div>
+			<Head>
+				<title>App-Goenaga</title>
+				<link rel="icon" href="/favicon.ico" />
+				<meta name="description" content="App-Goenaga" />
+			</Head>
+			<main>
+        <button
+          onClick={() => {
+			const newImage: Imageitem = {
+			  id: zorro(),
+			  url: `https://randomfox.ca/images/${zorro()}.jpg`,
+			};
+			const newImages = [newImage];
+            setImages([...images, ...newImages]);
+          }}
+          className="mb-4 p-2 bg-blue-500 text-white rounded"
+        >
+          Add Image
+        </button>
+				<h1 className="text-3xl font-bold">Random Fox</h1>
+				{images.map((image) => (
+					<div key={image.id} className="p-4">
+						<Randomfox image={image.url} />
+					</div>
+				))}
+			</main>
+			<footer></footer>
+		</div>
+	);
 };
 
 export default Home;
--- components/randomfox.tsx
+++ components/randomfox.tsx
@@ -1,10 +1,10 @@
 import { JSX } from "react";
-type Props = {image: string, zorro: number};
+type Props = {image: string};
 
 export const Randomfox = (props: Props): JSX.Element => {
 	return (
 		<div>
-			<img className="rounded" width={320} height="auto" src={props.image} alt={`Random Fox ${props.zorro}`} />
+			<img className="rounded" width={320} height="auto" src={props.image} alt="Random Fox"/>
 		</div>
 	);
 }
