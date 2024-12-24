b8132f078afb0c9031b614453f01f6f1ab6bd337
Luis Goenaga <luis.goenaga@est.iudigital.edu.co>
2024-12-23 19:52:36 -0500

    LazzyImage

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,20 @@
 "use client";
 
-import { useState, useEffect, MouseEventHandler} from "react";
+import { useState, useEffect, MouseEventHandler } from "react";
 import type { NextPage } from "next";
 import Head from "next/head";
-import { Randomfox } from "../components/randomfox";
-type Imageitem = { id: number,  url: string };
+import { LazyImage } from "../components/randomfox";
+type Imageitem = { id: number; url: string };
 
 const Home: NextPage = () => {
-const zorro = (): number => Math.floor(Math.random() * 123) + 1;
+	const zorro = (): number => Math.floor(Math.random() * 123) + 1;
 
-const [images, setImages] = useState<Imageitem[]>([]);
+	const [images, setImages] = useState<Imageitem[]>([]);
 
-useEffect(() => {
-	const img = `https://randomfox.ca/images/${zorro()}.jpg`;
-	setImages([{ id: zorro(), url: img }]);
-}, []);
+	useEffect(() => {
+		const img = `https://randomfox.ca/images/${zorro()}.jpg`;
+		setImages([{ id: zorro(), url: img }]);
+	}, []);
 
 	const addImage: MouseEventHandler<HTMLButtonElement> = (event) => {
 		event.preventDefault();
@@ -33,19 +33,24 @@ useEffect(() => {
 				<meta name="description" content="App-Goenaga" />
 			</Head>
 			<main>
-        <button
-		  		onClick={(event) => {
+				<button
+					onClick={(event) => {
 						addImage(event);
-		  			}
-					}
-          className="mb-4 p-2 bg-blue-500 text-white rounded"
-        >
-          Add Image
-        </button>
+					}}
+					className="mb-4 p-2 bg-blue-500 text-white rounded"
+				>
+					Add Image
+				</button>
 				<h1 className="text-3xl font-bold">Random Fox</h1>
 				{images.map((image) => (
 					<div key={image.id} className="p-4">
-						<Randomfox image={image.url} />
+						<LazyImage
+							image={image.url}
+							onClick={() => "Clic en la imagen"}
+							width={320}
+							height="auto"
+							className="rounded bg-gray-400"
+						/>
 					</div>
 				))}
 			</main>
--- components/randomfox.tsx
+++ components/randomfox.tsx
@@ -1,33 +1,39 @@
-import { JSX, useEffect, useRef, useState } from "react";
-type Props = {image: string};
+import { JSX, useEffect, useRef, useState, ImgHTMLAttributes } from "react";
+type LazyProps = { image: string };
+type ImageNative = ImgHTMLAttributes<HTMLImageElement>
+type Props = LazyProps & ImageNative;
 
-export const Randomfox = (props: Props): JSX.Element => {
+export const LazyImage = (props: Props): JSX.Element => {
 	const imageRef = useRef<HTMLImageElement>(null);
-	const [src, setSrc] = useState<string>("data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIwIiBoZWlnaHQ9IjMyMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2ZXJzaW9uPSIxLjEiLz4=");
-	useEffect (() => {
+	const [src, setSrc] = useState<string>(
+		"data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIwIiBoZWlnaHQ9IjMyMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2ZXJzaW9uPSIxLjEiLz4="
+	);
+	useEffect(() => {
 		const observer = new IntersectionObserver((entries) => {
 			entries.forEach((entry) => {
 				if (entry.isIntersecting) {
 					setSrc(props.image);
 					observer.disconnect();
 				}
-			}
-			);
-		}
-		);
+			});
+		});
 
-		if (imageRef.current)
-			observer.observe(imageRef.current as Element);
+		if (imageRef.current) observer.observe(imageRef.current as Element);
 
 		return () => {
 			observer.disconnect();
 		};
-
-	}, [ props.image ]);
+	}, [props.image]);
 
 	return (
 		<div>
-			<img className="rounded bg-gray-400" ref={imageRef} width={320} height="auto" src={src} alt="Random Fox"/>
+			<img
+				
+				ref={imageRef}
+				src={src}
+				alt="Random Fox"
+				{...props}
+			/>
 		</div>
 	);
-}
+};
